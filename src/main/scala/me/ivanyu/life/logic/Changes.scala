bf1c2272b9a27f9111b66e154a06ef1ebddfcf1c
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2015-04-28 16:19:58 +0300

    Initial

--- src/main/scala/me/ivanyu/life/logic/Changes.scala
+++
@@ -1,9 +0,0 @@
-package me.ivanyu.life.logic
-
-sealed case class Changes(cellPlane: CellPlane) extends CellPlaneContainer
-
-object Changes {
-  def apply(universe: Universe): Changes = {
-    Changes(universe.cellPlane)
-  }
-}
